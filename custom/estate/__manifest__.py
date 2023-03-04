10d76198fd26da6b06231d08b472deb7eeff8c79
Mario Riascos <marioriascos1201@gmail.com>
2023-03-04 11:18:02 -0500

    Chapter 1,2,3 Development environment setup, A New Application

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'author': 'Mario Fernando Riascos',
+    'maintainer': 'Mario Fernando Riascos',
+    'website': 'mfriascos.github.io',
+    'license': 'AGPL-3',
+    'depends': ['base']
+}
